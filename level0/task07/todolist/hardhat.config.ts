1c96298103491bf50460b2af9a04b89a14cb238e
黎小龙 <bluceli187@qq.com>
2025-09-03 09:59:11 +0800

    feat:连接并部署到 Ethereum网络

---
+++ level0/task07/todolist/hardhat.config.ts
@@ -0,0 +1,42 @@
+import type { HardhatUserConfig } from "hardhat/config";
+import dotenv from "dotenv";
+import hardhatToolboxMochaEthersPlugin from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
+import { configVariable } from "hardhat/config";
+dotenv.config();
+const config: HardhatUserConfig = {
+  plugins: [hardhatToolboxMochaEthersPlugin],
+  solidity: {
+    profiles: {
+      default: {
+        version: "0.8.28",
+      },
+      production: {
+        version: "0.8.28",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    },
+  },
+  networks: {
+    hardhatMainnet: {
+      type: "edr-simulated",
+      chainType: "l1",
+    },
+    hardhatOp: {
+      type: "edr-simulated",
+      chainType: "op",
+    },
+    sepolia: {
+      type: "http",
+      url:
+        "https://eth-sepolia.g.alchemy.com/v2/" + process.env.ALCHEMY_API_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+};
+
+export default config;
